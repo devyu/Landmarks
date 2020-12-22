605aaaff2947f4a82c7b2cd7168fb7446f00b392
Jingyu <jingyu.yang@huifu.com>
2020-12-22 18:38:13 +0800

    add navigation support

--- Landmarks/Views/Helpers/CircleImage.swift
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct CircleImage: View {
-    var body: some View {
-        Image("turtlerock")
-          .clipShape(Circle())
-          .overlay(Circle().stroke(Color.white, lineWidth: 4))
-          .shadow(radius: 7)
-    }
+  var image: Image
+  var body: some View {
+    image
+      .clipShape(Circle())
+      .overlay(Circle().stroke(Color.white, lineWidth: 4))
+      .shadow(radius: 7)
+  }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-      CircleImage()
+      CircleImage(image: Image("turtlerock"))
         .previewDevice("iPhone 12")
     }
 }
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    // add an @EnvironmentObject property declaration to the view, and an environmentObject(_:) modifier to the preview.
+    @EnvironmentObject var modalData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modalData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    
     var body: some View {
-//        Text("Hello, World!")
-      List {
-        LandmarkRow(landmark: landmarks[0])
-        LandmarkRow(landmark: landmarks[1])
+      NavigationView {
+        List {
+            // To combine static and dynamic views in a list, or to combine two or more different groups of dynamic views, use the ForEach type instead of passing your collection of data to List.
+            Toggle(isOn: $showFavoritesOnly, label: {
+                Text("Favorites only")
+            })
+            ForEach(filteredLandmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                  LandmarkRow(landmark: landmark)
+                }
+            }
+        }
+        .navigationTitle("Landmarks")
       }
     }
 }
@@ -20,6 +40,8 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
       LandmarkList()
+        .environmentObject(ModelData())
         .previewDevice("iPhone 12")
+        .previewDisplayName("iPhone 12")
     }
 }
