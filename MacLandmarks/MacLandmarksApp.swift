dc790cb44f4d3dca979dbf167b0415c5a98fd1a8
Jingyu <jingyu.yang@huifu.com>
2020-12-25 17:27:00 +0800

    macOS support

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Jingyu on 2020/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
